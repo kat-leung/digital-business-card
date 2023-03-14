b836857e51f2a1c312ecf4eeb54dd82e5ca17211
Kat L <kathyl123@gmail.com>
2023-03-13 18:45:19 -0700

    testing deployment

--- src/Body.js
+++ src/Body.js
@@ -4,7 +4,7 @@ export default function Body() {
     return (
         <main>
            <h1>kat leung</h1> 
-           <p className="title">web designer & developer</p>
+           <p className="title">web ssssdesigner & developer</p>
            <div className='buttons'>
                 <button><i class="fa fa-envelope" aria-hidden="true"></i>get in touch!</button>
                 <button><i class="fa fa-paint-brush" aria-hidden="true"></i>portfolio</button>
